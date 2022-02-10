4ed5f18d77ae578860c03f7c9940bb38af5bbf0b
Muhriddin Kamoljonov <muhriddinkamoljonov@gmail.com>
2022-02-10 14:59:17 +0500

    CArt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
 import Navbars from "./Components/Navbar/Navbars";
+import Card from "./Components/Cards/card";
 
 function App() {
     return (
         <div className="App">
             <Navbars/>
+            {/*<Card/>*/}
         </div>
     );
 }
