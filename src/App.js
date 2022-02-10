fc8d132f4f99198a437fba3eb313cffa5225b5ab
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-02-10 14:07:56 +0500

    hello world

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar/Navbar';
 function App() {
     return (
         <div className="App">
+            <h1>Hello World</h1>
             <Navbar/>
         </div>
     );
